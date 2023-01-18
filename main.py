ce8c6413e3a25471707e7bd64b36ea37763d6079
Smogg22 <ramadecha@gmail.com>
2023-01-18 12:29:19 +0100

    Arretez de casser les couilles

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 a=1
 b="salut petit patapon"
 c=" il était une bergère"
-<<<<<<< HEAD
 b="Bonjour petit patapon"
 
 if a > 7000:
     print("J'ai craqué le sac")
 
+print("Vous me faites chier les développeurs")
 
 test= "bonjour tout le monde"
 
 pourquoi je n'ai pas tout 
->>>>>>> e946903 (modifdefou)
+(modifdefou)
